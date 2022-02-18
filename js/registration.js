bb3e1ba8c65c2cec38e83295b5fa39538714c0e4
Karen Kuzmiak <lamar.clapham@gmail.com>
2022-02-18 14:03:35 -0800

    Updated registation page JS. Woking.

--- js/registration.js
+++ js/registration.js
@@ -1,31 +1,5 @@
-// window.onbeforeunload = function() { return "Your work will be lost."; };
-// document.addEventListener("deviceready", onDeviceReady(),false);
-// function onDeviceReady() {
-   
-//     console.log("Device Ready");
-
 window.onload = (event) => {
     console.log('Registration page is fully loaded');
-  
-
-    ////////////////// Setup Variables
-    const $elBtnLogIn = $("#loginBtn");
-    console.log($elBtnLogIn);
-
-    // Save form DB
-    // let myDB = new PouchDB("myComics");
-
-    //This is an array for checking the answers.
-    // const answerArr = [['A'], ['B']];
-    // var correctAnswers = 0;
-    // Save form Variables
-    // const $elmSaveComic = $('#cbSaveFrm');
-
-    /////////////////// Setup Listing Events
-    // User Login Form Event
-    $('#myLoginForm').submit(function () {
-        mainLogin();
-    });
 
     // User Signup FORM Listner
     $('#mySignUpForm').submit(function (event) {
@@ -33,77 +7,7 @@ window.onload = (event) => {
         fnSignUp(event);
     });
 
-    // User Logout Listner
-    $('#uLogoutBtn').click(function () {
-        uLogout();
-    })
 
-    $('#startBTN').click(function () {
-        window.location.replace("../Phishing_Quiz/question1.html")
-    })
-
-    $('.resultsBTN').click(function () {
-        console.log("Hitting the results button")
-        window.location.replace("../Phishing_Quiz/index.html#pgResults")
-    })
-
-    // User Button Listner
-    $('.answer-btn').click(function () {
-        questionNumber = parseInt(this.id);
-
-        calcAnswer(questionNumber);
-
-    })
-
-    // Save Page Reset Button To clear form
-    // $('#cbResetFrmBtn').click(function () {
-    //     // reset all field in save form
-    //     $('#cbSaveFrm')[0].reset();
-
-    // });
-
-    //Listen for view page to be selected and refresh list
-    // $('.navView').click(function () {
-    //     console.log('naveView is working');
-    //     fnViewComics();
-    // })
-    //Put it back here
-    // CB Save Form Listner
-
-
-    // Options Page Event Listeners
-    // $('#dataBaseDeleteBtn').click(function () {
-    //     console.log('dataBaseDeleteBtn is working');
-    //     deleteDB();
-    // });
-
-    //////////////////////// Functions mainLogin, Signup, Calculate, Results
-    function mainLogin(event) {
-        console.log("In mainLogain function " +event);
-        event.preventDefault(event);
-        console.log("mainLogin(event) is running");
-
-        let $elUserEmail = $("#inLoginEmail"),
-            $elUserLoginPw = $("#inUserPwLogin"),
-            $tmpValUserLoginEmail = $elUserEmail.val().toUpperCase(),
-            $tmpValUserLoginPW = $elUserLoginPw.val();
-
-        // Check to see if user has signed up
-        if (localStorage.getItem('dataKey') == null) {
-            window.alert("Please Sign Up");
-
-        } else {
-            let retrievedData = localStorage.getItem('dataKey');
-            let normalizedData = JSON.parse(retrievedData);
-
-            if ($tmpValUserLoginEmail === normalizedData['uEmail'] & ($tmpValUserLoginPW === normalizedData['uPassword'])) {
-                console.log("you should be at the home screen")
-                $(":mobile-pagecontainer").pagecontainer("change", "#pgHome");
-            } else {
-                window.alert('Login is incorrect');
-            }
-        }
-    };
 
     // Need to add a validation so the user must fill in all fields    
     function fnSignUp(event) {
@@ -125,58 +29,7 @@ window.onload = (event) => {
         // $(":mobile-pagecontainer").pagecontainer("change", "#pgInstructions");
     };
 
-    // Super Simple Logout---I think we should make this the submit score.
-    function uLogout() {
-        let result = window.confirm("Are you sure you want to logout?");
-        if (result = true) {
-            $(":mobile-pagecontainer").pagecontainer("change", "#pgWelcome");
-
-            // Reset the forms
-            $('#myLoginForm')[0].reset();
-            $('#mySignUpForm')[0].reset();
-        }
-    }
 };
-
-const correctAnswerArr = ['A','B']
-var answerArr = []
-var correctAnswers = 0;
-var numQuestions = 2;
-
-// This function will calculate the results of each question
-function calcAnswer(e) {
-    if (document.getElementById(e + '1').checked) {
-        answerArr[e] = "A"
-        console.log(answerArr)
-    } else if (document.getElementById(e + '2').checked) {
-        console.log("b")
-        answerArr[e] = "B"
-        console.log(answerArr)
-    }
-
-    if (answerArr[e] === correctAnswerArr[e]) {
-        alert("You got it right")
-        correctAnswers += 1
-        console.log("correct answers "+correctAnswers)
-    } else {
-        console.log("You got it wrong")
-    }
-
-    // document.getElementById('yourScore').innerHTML = correctAnswers;
-    
-}
-
-// function recordAnswers(e, correctAnswers) {
-//     indexes = parseint(numQuestions - 1);
-//     if (correctAnswerArr[e] === null) {
-//         correctAnswerArr[e] = e
-//     }
-
-// }
-
-
-
-
 
 // Simple return to last page -->
 function goBack() {
