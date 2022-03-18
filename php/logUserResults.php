29734f8826f18d36b58a99207e491487b8eb4485
Karen Kuzmiak <lamar.clapham@gmail.com>
2022-03-18 11:49:12 -0700

    updated PHP date file format.

--- php/logUserResults.php
+++ php/logUserResults.php
@@ -9,11 +9,19 @@ echo "Thank you for taking the Phish-Test.";
 echo "<br>";
 echo "You are prepared to defeat phishing attempts!";
 
+// create a $dt object with the UTC timezone
+$dt = new DateTime('2016-12-12 12:12:12', new DateTimeZone('UTC'));
+
+// change the timezone of the object without changing its time
+$dt->setTimezone(new DateTimeZone('America/Denver'));
+
+// format the datetime
+$dt->format('Y-m-d H:i:s T');
 
 
 $myfile = fopen("phishQuizLog.txt", "a") or die("Unable to open file!");
-$date = date('l jS \of F Y h:i:s A');
-fwrite($myfile, $date . " Score:" . $C . " Name:" . $A . " email:". $B ."\n");
+// $date = date('l jS \of F Y h:i:s A');
+fwrite($myfile, $dt . " Score:" . $C . " Name:" . $A . " email:". $B ."\n");
 fclose($myfile);
 // Notes
 ?>
