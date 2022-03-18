fd0af8833aa8fe609af044b9fb9eaea0091d9d71
Karen Kuzmiak <lamar.clapham@gmail.com>
2022-03-18 12:19:36 -0700

    updated php to ajust local time

--- php/logUserResults.php
+++ php/logUserResults.php
@@ -10,10 +10,11 @@ echo "<br>";
 echo "You are prepared to defeat phishing attempts!";
 
 // create a $dt object with the UTC timezone
-$dt = new DateTime('2016-12-12 12:12:12', new DateTimeZone('UTC'));
-
+// $dt = new DateTime('2016-12-12 12:12:12', new DateTimeZone('UTC'));
+// $newDT = new date('l jS \of F Y h:i:s A');
+$date = date('l jS \of F Y h:i:s A');
 // change the timezone of the object without changing its time
-$dt->setTimezone(new DateTimeZone('America/Los Angeles'));
+$dt->setTimezone(new $date('America/Los Angeles'));
 
 // format the datetime
 // $dt->format('Y-m-d H:i:s T');
@@ -21,7 +22,7 @@ $dt->setTimezone(new DateTimeZone('America/Los Angeles'));
 
 $myfile = fopen("phishQuizLog.txt", "a") or die("Unable to open file!");
 // $date = date('l jS \of F Y h:i:s A');
-fwrite($myfile, $dt->format('Y-m-d H:i:s T') . " Score:" . $C . " Name:" . $A . " email:". $B ."\n");
+fwrite($myfile, $dt . " Score:" . $C . " Name:" . $A . " email:". $B ."\n");
 fclose($myfile);
 // Notes
 ?>
